455befc7c0d6cd29a72e56e1e5442fc89a3c704e
malejandramunoz <maria.munoz10@upr.edu>
2020-07-01 11:03:25 -0400

    yes i was reading this

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
 import React from 'react';
 import './App.css';
 
-// eslint-disable-next-line 
+
 class ShoppingItem {
   public name = "";
   public price = 0.0;
 }
 
 interface AppState {
-  items: Array<string>;
+  items: Array<ShoppingItem>;
 }
 
 class App extends React.Component<{}, AppState> {
   constructor(props: any, state: AppState) {
     super(props, state);
-    this.state = {items: []};
+
+    const pizza: ShoppingItem = {
+      name: "Pizza",
+      price: 4.5
+    }
+    
+    const chips: ShoppingItem = {
+      name: "Chips",
+      price: 2.3
+    }
+
+    const soda: ShoppingItem = {
+      name: "Diet Soda",
+      price: 0.9
+    }
+
+    this.state = {items: [chips, pizza, soda]};
   }
 
   public render() {
     return (
       <div className="App">
         <header className="App-header">
+          Shopping List
           <h1>Testing</h1>
           <p>Test</p>
         </header>
