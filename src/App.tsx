bedf82013daa2709403c65db333884637dfb1c9b
malejandramunoz <maria.munoz10@upr.edu>
2020-07-01 10:58:50 -0400

    Constructor done

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Testing</h1>
-      </header>
-    </div>
-  );
+// eslint-disable-next-line 
+class ShoppingItem {
+  public name = "";
+  public price = 0.0;
+}
+
+interface AppState {
+  items: Array<string>;
+}
+
+class App extends React.Component<{}, AppState> {
+  constructor(props: any, state: AppState) {
+    super(props, state);
+    this.state = {items: []};
+  }
+
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Testing</h1>
+          <p>Test</p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
